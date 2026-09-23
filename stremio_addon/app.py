9b56e88cf09080136372b14b530bd0630a1e5767
hilayc <hilayc9@gmail.com>
2026-09-23 15:39:37 +0300

    Update manifest description for Telegram videos

--- stremio_addon/app.py
+++ stremio_addon/app.py
@@ -116,7 +116,7 @@ def create_app_with_runtime(runtime):
     async def manifest(key):
         auth(key)
         return {'id': 'community.private.telegram', 'version': get_version(), 'name': 'Private Telegram Videos',
-                'description': 'Your private Telegram videos with Hebrew and English search.',
+                'description': 'Stream your private Telegram videos',
                 'logo': 'https://raw.githubusercontent.com/hilayc/stremio-addon/main/stremio_addon/icon.png',
                 'types': ['movie', 'series'],
                 'resources': [{'name': 'catalog', 'types': ['movie']}, {'name': 'meta', 'types': ['movie'], 'idPrefixes': ['tg:']}, {'name': 'stream', 'types': ['movie', 'series'], 'idPrefixes': ['tg:', 'tt']}],
